097891b1addd49305cc6c3526f03f0ccf1c41e74
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2022-02-20 15:05:09 -0500

    refactored App component for theme switching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './sections/Navbar/Navbar.js';
 import Landing from './sections/Landing/Landing.js';
 import About from './sections/About/About.js';
@@ -9,20 +11,46 @@ import Speakers from "./sections/Speakers/Speakers";
 import Purpose from "./sections/Purpose/Purpose";
 import Sponsors from "./sections/Sponsors/Sponsors";
 
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/Globalstyle";
+import { lightTheme, darkTheme } from "./components/Themes" 
+import  {useDarkMode} from "./components/useDarkMode"
+import Toggle from "./components/Toggler"
+
 
 function App() {
 
+  const [theme, themeToggler] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  // const [theme, setTheme] = useState('light');
+  // const themeToggler = () => {
+  //   theme === 'light' ? setTheme('dark') : setTheme('light')
+  // }
+
     return (
-        <div>
-            {window.innerWidth >= 768 && <Navbar/>}
-            <Landing/>
-            <About/>
-            <Purpose/>
-            <Schedule/>
-            <Speakers/>
-            <Sponsors/>
-            <FAQ/>
-        </div>
+
+      <ThemeProvider theme={themeMode}>
+        <>
+          <GlobalStyles/>
+
+            {/* <button onClick={themeToggler}>Switch Theme</button> */}
+
+
+              <Router>
+                  <div className="navbar-container">
+                    <Navbar/>
+                    <Toggle theme={theme} toggleTheme={themeToggler} />
+                  </div>
+                    <Switch>
+                      <Route exact path="/">
+                      <Landing/>
+                      </Route>
+                    </Switch>
+              </Router>
+        </>
+      </ThemeProvider>
     );
 }
 
