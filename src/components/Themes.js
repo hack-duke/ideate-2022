866710250476f2af01978165e092cc6c8d52f86f
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2022-02-20 14:57:29 -0500

    added a dark/light theme component

---
+++ src/components/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: '#FFF',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#0A112B',
+}
+export const darkTheme = {
+    body: '#0A112B',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#FFF',
+}
