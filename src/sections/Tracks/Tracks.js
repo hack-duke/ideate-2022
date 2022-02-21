47f57e99bae816a4fab5a03e4863d18e51e60e63
Johnny Pesavento <jpes707@gmail.com>
2022-02-20 23:30:40 -0500

    Added tracks component

--- src/sections/Tracks/Tracks.js
+++ src/sections/Tracks/Tracks.js
@@ -1,39 +1,38 @@
 import React from "react";
 import './Tracks.css';
 
-import tr from '../../graphics/schedule-top-right.svg';
-import bl from '../../graphics/schedule-bottom-left.svg';
-import br from '../../graphics/schedule-bottom-right.svg';
-
+import top from '../../graphics/assets/tracks/tracks-top.svg';
+import bottom from '../../graphics/assets/tracks/tracks-bottom.svg';
+import health from '../../graphics/assets/tracks/health-icon.svg';
+import inequality from '../../graphics/assets/tracks/inequality-icon.svg';
+import education from '../../graphics/assets/tracks/education-icon.svg';
 
 function Tracks() {
     return (
-        <div id="schedule">
+        <div id="tracks">
             <div id="blobs">
-                <img id="top-right" src={tr} alt="blob"></img>
-                <img id="top-left" src={bl} alt="blob"></img>
-                <img id="bottom-right" src={br} alt="blob"></img>
+                <img id="top" src={top} alt="blob"></img>
             </div>
-
-            <div className={"schedule-container"}>
-                <h1>Schedule</h1>
-                <div className="days">
-                    <div className="day">
-                        <h3>Saturday April 10</h3>
-                        <p>10am       Opening and Keynote Presentation</p>
-                        <p>12pm       Workshops and Talks</p>
-                        <p>5pm         Design Challenge Begins</p>
-                        <p>6pm         Nonprofit Q&As and Office Hours</p>
-                        <p>9pm         Virtual Game Night</p>
+            <div className={"tracks-container"}>
+                <h1>TRACKS</h1>
+                <div className="track-table">
+                    <div className="track">
+                        <img id="health" src={health}></img>
+                        <h3>Health</h3>
+                        <p>More than ever before, technology today is in a prime position to improve the quality, efficiency, and accessibility of health care. How can we design applications and technologies to support frontline workers, aid populations at higher risk, and address the health concerns that have emerged throughout this pandemic?</p>
                     </div>
-                    <div className="day">
-                        <h3>Sunday April 11</h3>
-                        <p>10am        Bob Ross MS Paint Event</p>
-                        <p>3:30pm    Submissions Due</p>
-                        <p>4pm         Judging Begins</p>
-                        <p>7pm         Closing and Awards Ceremony</p>
+                    <div className="track">
+                        <img id="inequality" src={inequality}></img>
+                        <h3>Inequality</h3>
+                        <p>While technology has the potential to reduce social disparities, it has more frequently further exemplified them. Facial recognition, surveillance, and other technologies disproportionately profile and incarcerate Black people. How can we design technology that supports and uplifts marginalized communities and remove the technology that harms them?</p>
+                    </div>
+                    <div className="track">
+                        <img id="education" src={education}></img>
+                        <h3>Education</h3>
+                        <p>Transitioning to online learning hasn't been easy. How can we design solutions to help those struggling with learning in difficult home environments, create tools for classes transitioning to online formats, and replicate the learning experience at home?</p>
                     </div>
                 </div>
+                <img id="bottom" src={bottom} alt="blob"></img>
             </div>
         </div>
     )
