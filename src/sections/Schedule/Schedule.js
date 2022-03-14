c1556ec136729a048fcb232949316e0f86271dd7
Jacob Liu <52381122+LiuJ2020@users.noreply.github.com>
2022-03-14 18:53:59 -0400

    Schedule Page added – need to fix the fact that the background repeats weirdly.

--- src/sections/Schedule/Schedule.js
+++ src/sections/Schedule/Schedule.js
@@ -1,32 +1,40 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './Schedule.css';
 
 import clouds from '../../graphics/schedule-top-right.svg';
-
+import 'react-tabs/style/react-tabs.css';
 
 function Schedule() {
     return (
+
         <div id="schedule">
             <div className={"schedule-container"}>
                 <h1 id="schedule-title">Schedule</h1>
+                <Tabs>
+                    <TabList>
+                        <Tab><h3>Saturday April 10</h3></Tab>
+                        <Tab><h3>Sunday April 11</h3></Tab>
+                    </TabList>
+                    <TabPanel>
+                        <div className="day">
+                            <p>10am       Opening and Keynote Presentation</p>
+                            <p>12pm       Workshops and Talks</p>
+                            <p>5pm         Design Challenge Begins</p>
+                            <p>6pm         Nonprofit Q&As and Office Hours</p>
+                            <p>9pm         Virtual Game Night</p>
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="day">
 
-                <div className="days">
-                    <div className="day">
-                        <h3>Saturday April 10</h3>
-                        <p>10am       Opening and Keynote Presentation</p>
-                        <p>12pm       Workshops and Talks</p>
-                        <p>5pm         Design Challenge Begins</p>
-                        <p>6pm         Nonprofit Q&As and Office Hours</p>
-                        <p>9pm         Virtual Game Night</p>
-                    </div>
-                    <div className="day">
-                        <h3>Sunday April 11</h3>
-                        <p>10am        Bob Ross MS Paint Event</p>
-                        <p>3:30pm    Submissions Due</p>
-                        <p>4pm         Judging Begins</p>
-                        <p>7pm         Closing and Awards Ceremony</p>
-                    </div>
-                </div>
+                            <p>10am        Bob Ross MS Paint Event</p>
+                            <p>3:30pm    Submissions Due</p>
+                            <p>4pm         Judging Begins</p>
+                            <p>7pm         Closing and Awards Ceremony</p>
+                        </div>
+                    </TabPanel>
+                </Tabs>
             </div>
         </div>
     )
