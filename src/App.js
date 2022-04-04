812a7619c9724503dd25c6fec5900ff9e9b8d5c8
Graham Gumbert <gjgumbert@gmail.com>
2022-04-04 18:13:23 -0400

    always show navbar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <div>
-            {window.innerWidth >= 768 && <Navbar/>}
+            <Navbar/>
             <Landing/>
             <About/>
             <Tracks/>
